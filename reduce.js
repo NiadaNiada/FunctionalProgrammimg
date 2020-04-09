2e30dcfd06738e4e5986309f6dc8600e6102fa2c
niadaniada <diana.arsenii@icloud.com>
2020-04-09 11:55:05 +0300

    modified reduce.js

--- reduce.js
+++ reduce.js
@@ -1,130 +1,119 @@
-let arrayOne = [1, 2, 3, 4];
+let arrayOne = [1, 3, 2, 4];
 let arrayTwo = [4, 6, 8];
 const even = element => element % 2 === 0;
 
 const concat = (array, newElement) => {
     return array.reduce(() => {
-        return [...array, ...newElement];
+        return Array.isArray(newElement)
+            ? [...array, ...newElement]
+            : [...array, newElement];
     });
 };
 
+console.log(concat(arrayOne, 3));
 console.log(concat(arrayOne, arrayTwo));
 
 const filter = (array, filterFunction) => {
-    return array.reduce(() => {
-        let filterArr = [];
-        for (let elem of array) {
-            if (filterFunction(elem)) {
-                return filterArr.push(array[elem]);
-            }
+    return array.reduce((accum, curVal) => {
+        if (filterFunction(curVal)) {
+            return [...accum, curVal];
         }
-        return undefined;
-    });
+        return accum;
+    }, []);
 };
+
 console.log(filter(arrayTwo, even));
 
-const find = (array, func) => {
-    array.reduce(() => {
-        for (let elem of array) {
-            if (func(elem)) {
-                return elem;
-            }
+const find = (array, findFunc) => {
+    return array.reduce((accum, curVal) => {
+        if (accum !== undefined) {
+            return accum;
         }
-        return undefined;
-    });
+        if (findFunc(curVal)) {
+            return curVal;
+        }
+        return accum;
+    }, undefined);
 };
 
 console.log(find(arrayOne, even));
 
 const findIndex = (array, condition) => {
-    return array.reduce(() => {
-        for (let index in array) {
-            if (condition(array[index])) {
-                return index;
-            }
+    return array.reduce((accum, curVal, index) => {
+        if (accum < 0 && condition(curVal)) {
+            return index;
         }
-        return undefined;
-    });
+        return accum;
+    }, -1);
 };
 
-console.log(findIndex(arrayOne, even));
+console.log(findIndex(arrayTwo, even));
 
 const includes = (array, element) => {
-    return array.reduce(() => {
-        for (let elem of array) {
-            if (elem === element) {
-                return true;
-            }
+    return array.reduce((accum, curVal) => {
+        console.log(accum, curVal);
+        if (curVal === element) {
+            return true;
         }
-        return false;
-    });
+        return accum;
+    }, false);
 };
 
 console.log(includes(arrayOne, 3));
 
 const indexOf = (array, element) => {
-    return array.reduce(() => {
-        for (let elem in array) {
-            if (array[elem] === element) {
-                return elem;
-            }
+    return array.reduce((accum, curVal, index) => {
+        if (accum < 0 && curVal === element) {
+            return index;
         }
-        return undefined;
-    });
+        return accum;
+    }, -1);
 };
 
 console.log(indexOf(arrayOne, 3));
 
 const map = (array, mapFunction) => {
-    return array.reduce(() => {
-        const mapArr = [];
-        for (let i = 0; i < array.length; i++) {
-            const result = mapFunction(array[i]);
-            mapArr.push(result);
-        }
-        return mapArr;
-    });
+    return array.reduce((accum, curVal) => {
+        console.log(accum, curVal);
+        return [...accum, mapFunction(curVal)];
+    }, []);
 };
 
 console.log(map(arrayTwo, item => item / 2));
 
 const reverse = array => {
-    return array.reduce((acc, curr, index, arr) => {
-        acc[arr.length - index - 1] = curr;
-        return acc;
+    return array.reduce((accum, curVal, index) => {
+        accum[array.length - index - 1] = curVal;
+        console.log(curVal);
+        return accum;
     }, []);
 };
 
 const reverse1 = array => {
-    return array.reduce((a, b) => [b].concat(a));
+    return array.reduce((accum, curVal) => [curVal].concat(accum));
 };
 
 console.log(reverse1(arrayOne));
-
 console.log(reverse(arrayOne));
 
 const some = (array, condition) => {
-    return array.reduce(() => {
-        for (let elem of array) {
-            if (condition(elem)) {
-                return true;
-            }
+    return array.reduce((accum, curVal) => {
+        if (condition(curVal)) {
+            return true;
         }
-        return false;
-    });
+        return accum;
+    }, false);
 };
 
 console.log(some(arrayOne, even));
 
 const every = (array, condition) => {
-    return array.reduce(() => {
-        for (let elem of array) {
-            if (!condition(elem)) {
-                return false;
-            }
+    return array.reduce((accum, curVal) => {
+        if (accum) {
+            return condition(curVal);
         }
-        return true;
-    });
+        return accum;
+    }, true);
 };
 
 console.log(every(arrayTwo, even));
